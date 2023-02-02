b0d03d50f246b0401849f3f2cb843b95d73d5d47
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-02 12:54:15 -0500

    adding auth services

---
+++ src/app/core/Auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from './auth.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  users: User[] = [
+    { name: 'Pedro', email: 'pedrojose0272@gmail.com', password: '123456' },
+  ];
+
+  register(user: User) {
+    this.users.push(user);
+
+    return {
+      status: 201,
+      data: user,
+      msg: 'User Created',
+    };
+  }
+
+  login({ email, password }: any) {
+    const found = this.users.find(
+      (user) => user.email === email && password === user.password
+    );
+
+    if (found)
+      return {
+        status: 200,
+        data: found,
+        msg: 'Login successful',
+      };
+    return {
+      status: 404,
+      error: 'user not found',
+    };
+  }
+}
